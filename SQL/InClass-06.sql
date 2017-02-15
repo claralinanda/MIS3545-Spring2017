05203345159f19cfb1c857245b06e1371e1239e4
Zhi Li <zli@babson.edu>
2017-02-15 14:52:08 -0500

    Finish Session 06

--- SQL/InClass-06.sql
+++ SQL/InClass-06.sql
@@ -1,16 +1,19 @@
 use AdventureWorks2012;
 
-/*Activity 1. Using the HumanResource.Employee table, provide a count of the number of employees by job title.  The query should consider only current employees (the CurrentFlag must equal 1).  */
-
-
-
-
+/*Activity 1. Using the HumanResources.Employee table, provide a count of the number of employees by job title.  The query should consider only current employees (the CurrentFlag must equal 1).  */
+SELECT JobTitle, Count(BusinessEntityID) as Number
+FROM HumanResources.Employee
+WHERE CurrentFlag = 1
+GROUP BY JobTitle
+ORDER BY Count(BusinessEntityID) DESC;
 
 /*Activity 2. Modify the query you created in Activity 1 so that the output shows only those job titles for which there is more than 1 employee.  */
-
-
-
-
+SELECT JobTitle, Count(BusinessEntityID) as Number
+FROM HumanResources.Employee
+WHERE CurrentFlag = 1
+GROUP BY JobTitle
+HAVING Count(BusinessEntityID) > 1
+ORDER BY Count(BusinessEntityID) DESC;
 
 /*Activity 3. For each product, show its ProductID and Name (FROM the ProductionProduct table) and the location of its inventory (FROM the Product.Location table) and amount of inventory held at that location (FROM the Production.ProductInventory table).*/
 
