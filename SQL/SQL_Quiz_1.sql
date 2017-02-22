b8ce34884abcabef3d563f8e2c385223a5818568
Zhi Li <zli@babson.edu>
2017-02-22 13:13:43 -0500

    SQL quiz 1

---
+++ SQL/SQL_Quiz_1.sql
@@ -0,0 +1,20 @@
+USE AdventureWorks2012; /*Set current database*/
+
+/*1. Show card type and number of customers for each type of credit cards.*/
+/* Tables: Sales.CreditCard, Sales.PersonCreditCard*/
+
+
+
+/*2. Show the product ID, product name, and list price for each product where the list price is higher than the average standard cost for all products. */
+/* Table: Production.Product*/
+
+
+
+
+/*3. Show the product category name, subcategory name, product name, and product ID for all products that belong to a category and subcategory. These should be sorted alphabetically by category name, within category by subcategory name, and within subcategory by product name. Note: your result set should produce a total of 295 rows. */
+/* Tables: Production.Product, Production.ProductSubcategory, Production.ProductCategory*/
+
+
+
+
+/*4. Modify the query above (query 3) so that it includes only those products that do not belong to a category or subcategory. Note: your result set should produce a total of 209 rows. */
