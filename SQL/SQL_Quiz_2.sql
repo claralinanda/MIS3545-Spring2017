1918315f724962b8f47e1d7d02a0c28c60132c92
Zhi Li <zli@babson.edu>
2017-03-06 13:52:49 -0500

    add sql quiz 2

---
+++ SQL/SQL_Quiz_2.sql
@@ -0,0 +1,13 @@
+use AdventureWorksDW2012;
+
+
+/*1, Display number of orders and total sales amount(sum of SalesAmount) of Internet Sales in 1st quarter each year in each country. Note: your result set should produce a total of 18 rows. */
+
+
+/*2, Show total reseller sales amount (sum of SalesAmount), calendar quarter of order date, product category name and reseller’s business type by quarter by category and by business type in 2006. Note: your result set should produce a total of 44 rows. */
+
+
+/*3, Based on 2, perform an OLAP operation: slice. In the comment block, describe how you perform the slicing. */
+
+
+/*4, Based on 2, perform an OLAP operation: drill-down. In the comment block, describe how you perform the drill-down. */
